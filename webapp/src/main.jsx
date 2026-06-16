bb0f5fb4333c4435071eecce482b9a39eee25edb
officiallygod <allen.officially@gmail.com>
2026-06-16 08:46:21 +0200

    fix: Resolve Tailwind and HeroUI build errors

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './index.css'
 import App from './App.jsx'
 
-import { HeroUIProvider } from "@heroui/react";
+import { HeroUIProvider } from "@heroui/system";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

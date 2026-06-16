e6f8a2d3013e8ef8968a1be77c719c5d4e13a974
officiallygod <allen.officially@gmail.com>
2026-06-16 08:47:35 +0200

    fix: Revert to Tailwind CSS v3 for HeroUI compatibility

---
+++ webapp/tailwind.config.js
@@ -0,0 +1,18 @@
+const { heroui } = require("@heroui/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [heroui()],
+}
